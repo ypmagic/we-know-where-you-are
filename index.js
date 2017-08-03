637d91a5479390b13bbd9a261f2a2f4b32bc1e59
Young <young_park@brown.edu>
2017-08-03 14:52:19 +0000

    fixed once more

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     app = express(),
     request = require("request");
+let server = require('http').Server(app);
     
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
@@ -52,6 +53,7 @@ app.get("/", function(req, res) {
     });
 });
 
-app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+    console.log("App is running on port " + port);
 });
