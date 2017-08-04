7d7e952eba2ec5f2148e1982b17606c81c9e2a54
Young <young_park@brown.edu>
2017-08-04 01:39:51 +0000

    fixed it

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express"),
     app = express(),
-    request = require("request");
-let server = require('http').Server(app);
-var io = require('socket.io').listen(server);
+    request = require("request"),
+    server = require('http').Server(app),
+    io = require('socket.io').listen(server);
     
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
@@ -14,55 +14,64 @@ app.use(express.static(__dirname + '/public'));
 //     })
 // }
 
-var address;
-io.sockets.on('connection', function (socket) {
-  address = socket.handshake.address;
-  console.log('New connection from ' + address.address + ':' + address.port);
+function getCity(ip, callback) {
+    var city = "";
+    var regionName = "";
+    request("http://ip-api.com/json/" + ip, function(error, response, body) {
+        var parsed = JSON.parse(body);
+        city += parsed["city"];
+        regionName += parsed["regionName"];
+        callback({
+            city: city,
+            regionName: regionName
+        })
+        return;
+    });
+}
+
+app.get("/", function(req, res) {
+    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+    getCity(ip, function(response) {
+        var geoRequest = response;
+        var extract = "";
+        var wikiText = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"], function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var parsed = JSON.parse(body);
+                var pageId = parsed["query"]["pageids"][0];
+                extract = parsed["query"]["pages"][pageId]["extract"];
+                if (extract.includes("refer") || extract.includes("alternative") || extract.includes("refers") || extract.includes("several")) {
+                    var secondReq = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"] + ", " + geoRequest["regionName"], function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var parsed = JSON.parse(body);
+                            var pageId = parsed["query"]["pageids"][0];
+                            extract = parsed["query"]["pages"][pageId]["extract"];
+                            res.render("index", {
+                                city: geoRequest["city"] + ", " + geoRequest["regionName"],
+                                text: extract
+                            });
+                        }
+                    }); 
+                } else {
+                    res.render("index", {
+                        city: geoRequest["city"],
+                        text: extract
+                    });
+                }
+            }
+        });
+    })
 });
 
 app.get("/", function(req, res) {
-    var regionName = "";
-    var geoRequest = request("http://ip-api.com/json/" + address, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var parsed = JSON.parse(body);
-            geoRequest = parsed["city"]; 
-            regionName = parsed["regionName"];
-        } 
-    });
-    var extract = "";
-    var wikiText = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var parsed = JSON.parse(body);
-            var pageId = parsed["query"]["pageids"][0];
-            extract = parsed["query"]["pages"][pageId]["extract"];
-            if (extract.includes("refer") || extract.includes("alternative") || extract.includes("refers") || extract.includes("several")) {
-                geoRequest += ", " + regionName;
-                var secondReq = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest, function(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        var parsed = JSON.parse(body);
-                        var pageId = parsed["query"]["pageids"][0];
-                        extract = parsed["query"]["pages"][pageId]["extract"];
-                        res.render("index", {
-                            city: geoRequest,
-                            text: extract
-                        });
-                    }
-                }); 
-            } else {
-                res.render("index", {
-                    city: geoRequest,
-                    text: extract
-                });
-            }
-        }
-    });
+    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+
 });
 
-var port = process.env.PORT || 8000;
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
-
-// app.listen(process.env.PORT, process.env.IP, function() {
-//     console.log("The server has started.");
+// var port = process.env.PORT || 8000;
+// server.listen(port, function() {
+//     console.log("App is running on port " + port);
 // });
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The server has started.");
+});
