b932dc123110ba17c2e374a25d54a9d0b1ca8728
Young <young_park@brown.edu>
2017-08-04 00:31:09 +0000

    added socket

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app = express(),
     request = require("request");
 let server = require('http').Server(app);
+var io = require('socket.io').listen(server);
     
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
@@ -13,23 +14,21 @@ app.use(express.static(__dirname + '/public'));
 //     })
 // }
 
-var regionName = "";
-var geoRequest = request("http://ip-api.com/json/", function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var parsed = JSON.parse(body);
-        geoRequest = parsed["city"]; 
-        regionName = parsed["regionName"];
-    } 
+var address;
+io.sockets.on('connection', function (socket) {
+  address = socket.handshake.address;
+  console.log('New connection from ' + address.address + ':' + address.port);
 });
 
 app.get("/", function(req, res) {
-    // var ipAddr = req.headers["x-forwarded-for"];
-    // if (ipAddr){
-    //     var list = ipAddr.split(",");
-    //     ipAddr = list[list.length-1];
-    // } else {
-    //     ipAddr = req.connection.remoteAddress;
-    // }
+    var regionName = "";
+    var geoRequest = request("http://ip-api.com/json/" + address, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var parsed = JSON.parse(body);
+            geoRequest = parsed["city"]; 
+            regionName = parsed["regionName"];
+        } 
+    });
     var extract = "";
     var wikiText = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest, function(error, response, body) {
         if (!error && response.statusCode == 200) {
