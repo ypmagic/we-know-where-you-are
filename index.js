83c1ee4b9c90ea01a1647cea1032f636efa8c085
Young <young_park@brown.edu>
2017-08-04 03:30:40 +0000

    refactored code

--- index.js
+++ index.js
@@ -7,29 +7,54 @@ var express = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
-// var weirdCities = ["refer", "alternative", "refers"];
-// function containsWeird(extract) {
-//     weirdCities.forEach(function(weird) {
-//         return extract.includes(weird);
-//     })
-// }
+var weirdCities = ["refer", "alternative", "refers"];
 
 function getCity(ip, callback) {
     var city = "";
     var regionName = "";
     var countryCode = "";
+    var countryName = "";
     request("https://freegeoip.net/json/" + ip, function(error, response, body) {
         var parsed = JSON.parse(body);
         city += parsed["city"];
         regionName += parsed["region_name"];
         countryCode += parsed["country_code"];
+        countryName += parsed["country_name"];
         callback({
             city: city,
             regionName: regionName,
-            countryCode: countryCode
+            countryCode: countryCode,
+            countryName: countryName
         })
         return;
     });
+}
+
+function getWikiQuery(body, extract, geoRequest, callback) {
+    if (body["query"].hasOwnProperty(["redirects"])) {
+        callback("https://en.wikipedia.org/w/api.php?format=json&action=query&redirects&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + body["query"]["redirects"][0]["to"])
+    } else {
+        if (geoRequest["countryCode"] === "US") {
+            callback("https://en.wikipedia.org/w/api.php?format=json&action=query&redirects&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"] + ", " + geoRequest["regionName"]);
+        } else {
+            callback("https://en.wikipedia.org/w/api.php?format=json&action=query&redirects&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"] + ", " + geoRequest["countryName"]);  
+        }
+    }
+    return;
+}
+
+function containsWeird(extract, callback) {
+    var containsOne = false;
+    if (extract === "") {
+        containsOne = true;
+    }
+    weirdCities.forEach(function(weird) {
+        if (extract.includes(weird)) {
+            containsOne = true;
+        }
+    })
+    callback(containsOne);
+    return;
 }
 
 app.get("/", function(req, res) {
@@ -37,29 +62,41 @@ app.get("/", function(req, res) {
     getCity(ip, function(response) {
         var geoRequest = response;
         var extract = "";
-        var wikiText = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"], function(error, response, body) {
+        var wikiText = request("https://en.wikipedia.org/w/api.php?format=json&action=query&redirects&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"], function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var parsed = JSON.parse(body);
                 var pageId = parsed["query"]["pageids"][0];
                 extract = parsed["query"]["pages"][pageId]["extract"];
-                if (geoRequest["countryCode"] === "US" && (extract.includes("refer") || extract.includes("alternative") || extract.includes("refers") || extract.includes("several") || extract === "")) {
-                    var secondReq = request("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&indexpageids&exintro=&explaintext=&titles=" + geoRequest["city"] + ", " + geoRequest["regionName"], function(error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            var parsed = JSON.parse(body);
-                            var pageId = parsed["query"]["pageids"][0];
-                            extract = parsed["query"]["pages"][pageId]["extract"];
-                            res.render("index", {
-                                city: geoRequest["city"] + ", " + geoRequest["regionName"],
-                                text: extract
-                            });
-                        }
-                    }); 
-                } else {
-                    res.render("index", {
-                        city: geoRequest["city"],
-                        text: extract
-                    });
-                }
+                containsWeird(extract, function(response) {
+                   if (response) {
+                       getWikiQuery(parsed, extract, geoRequest, function(response) {
+                          var secondRequestUrl = response;
+                          var secondRequest = request(secondRequestUrl, function(error, response, body) {
+                             if (!error && response.statusCode == 200) {
+                                 parsed = JSON.parse(body);
+                                 pageId = parsed["query"]["pageids"][0];
+                                 extract = parsed["query"]["pages"][pageId]["extract"];
+                                 if (geoRequest["countryCode"] == "US") {
+                                     res.render("index", {
+                                        city: geoRequest["city"] + ", " + geoRequest["regionName"],
+                                        text: extract
+                                     });
+                                 } else {
+                                     res.render("index", {
+                                        city: geoRequest["city"] + ", " + geoRequest["countryName"],
+                                        text: extract
+                                     });
+                                 }
+                             } 
+                          });
+                       });
+                   } else {
+                       res.render("index", {
+                           city: geoRequest["city"],
+                           text: extract
+                       })
+                   }
+                });
             }
         });
     })
